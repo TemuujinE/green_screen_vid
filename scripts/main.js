1d25e860765c4e0bfcf49dba9a035bfd7656e17c
RazyDave <cdave503@gmail.com>
2022-02-15 16:01:31 +0800

    changed x rotation

--- scripts/main.js
+++ scripts/main.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const material = createChromaMaterial(texture, 0x00ff00); // 0x00ff00: Green screen removal
     const plane = new THREE.Mesh(geometry, material);
 
-    plane.rotation.x = Math.PI/2;
+    //plane.rotation.x = Math.PI/2;
     plane.position.y = 0.7;
     plane.scale.multiplyScalar(2);
 
